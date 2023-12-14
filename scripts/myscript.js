df8c788335d034f332c130ade527f43aa17b8dce
Emmanuel Murungi Mwebaze <emmanuelmurungimwebaze@Emmanuels-MacBook-Pro.local>
2023-12-14 17:08:40 -0500

    Changed source of D3 graph to be from json in Github bucket

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,7 @@
 // add your JavaScript/D3 to this file
 
 
-d3.json("http://localhost:8000/earthquake_data.json").then(function(data) {
+d3.json("https://raw.githubusercontent.com/aa221/EDAV-EARTHQUAKES-PROJECT/main/earthquake_data.json").then(function(data) {
   const margin = { top: 30, right: 30, bottom: 50, left: 50 };
   const width = 800 - margin.left - margin.right;
   const height = 600 - margin.top - margin.bottom;
